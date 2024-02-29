e04a3d09b21f3c988bda5baca29b43853b559a7a
A <38929533+20142995@users.noreply.github.com>
2024-02-29 19:53:10 +0800

    Update run.py

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ class GithubClient:
     def __init__(self, token):
         self.url = 'https://api.github.com'
         self.headers = {
-            'Authorization': f'Bearer {token}',
+            'Authorization': f'{token}',
             'Connection': 'close',
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
         }
